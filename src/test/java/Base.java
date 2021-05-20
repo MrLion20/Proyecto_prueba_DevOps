223ee3216b75157d7040f71c865429aa9cf4b3b6
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-20 10:10:54 -0600

    Se Crea la Clase Base, en donde se generan las funciones para los diferentes objetos que se van a necesitar para la Prueba.

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,2 +1,58 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class Base {
+    protected WebDriver driver;
+
+    public Base(WebDriver driver) {
+        this.driver = driver;
+    }
+    // aca se llama el ChromeConnection para que se pueda utilizar llamando las paginas que se van a probar
+    public WebDriver chromeDriverConnection() {
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        return driver;
+    }
+    // Aca se definen variables como la locator para que sea mas sencilla la busqueda de los elementos en los test.
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements(By locator){
+        return driver.findElements(locator);
+    }
+    public String getText(WebElement element) {
+        return element.getText();
+    }
+
+    public String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    // Aca se le asigna la palabra Type al sendkeys cuando queremos escribir en algun espacio de text
+    public void type(String inputText, By locator) {
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    //Aca se menciona tambien la accion de dar clic a un elemento
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    //Esta funcion lo que realiza es verificar que el locator(elemento) que estamos buscando este disponible para aplicar la accion deseada.
+    public Boolean isDisplayed(By locator) {
+        try {
+            return driver.findElement(locator).isDisplayed();
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+    public void visit(String url) {
+        driver.get(url);
+    }
 }
