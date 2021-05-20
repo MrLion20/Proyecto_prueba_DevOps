06e0d8aa07501d364d056f361f08e0fd958111d9
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-20 10:14:58 -0600

    Se agrega Clase Conexion

---
+++ src/test/java/Conexion.java
@@ -0,0 +1,6 @@
+public class Conexion {
+    //Accion donde se asigna el sitio a probar
+    final static String BASE_URL = "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F";
+    final static String CHROME_DRIVER_LOCATION = "chromedriver";
+}
+
