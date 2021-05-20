389dea86e9aae4ca2913d7d7ed75535557e75b41
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-20 10:27:49 -0600

    Se Actualiza la Clase Base

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-
 public class Base {
 
     protected WebDriver driver;
@@ -14,42 +12,40 @@ public class Base {
         PageFactory.initElements(driver, this);
     }
 
-    // Aca se definen variables como la locator para que sea mas sencilla la busqueda de los elementos en los test.
-
+    //Para buscar los elementos
     public WebElement findElement(By locator) {
         return driver.findElement(locator);
     }
 
-    public List<WebElement> findElements(By locator){
-        return driver.findElements(locator);
-    }
-    public String getText(WebElement element) {
-        return element.getText();
-    }
-
-    public String getText(By locator) {
-        return driver.findElement(locator).getText();
-    }
-
-    // Aca se le asigna la palabra Type al sendkeys cuando queremos escribir en algun espacio de text
-    public void type(String inputText, By locator) {
-        driver.findElement(locator).sendKeys(inputText);
-    }
-
-    //Aca se menciona tambien la accion de dar clic a un elemento
-    public void click(By locator) {
+    //Accion para dar clic a un elemento
+    public void click(By locator){
         driver.findElement(locator).click();
     }
 
-    //Esta funcion lo que realiza es verificar que el locator(elemento) que estamos buscando este disponible para aplicar la accion deseada.
-    public Boolean isDisplayed(By locator) {
-        try {
+    //Accion para dar limpiar textbox
+    public void clear(By locator){
+        driver.findElement(locator).clear();
+    }
+
+    //Accion para mandar texto
+    public void type(String inputText, By locator){
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    //Accion para tomar text
+    public String getText(WebElement element) {
+        return element.getText();
+    }
+    public String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+    //Accion para verificar la disponibilidad de un elemento
+
+    public boolean isDisplayed(By locator){
+        try{
             return driver.findElement(locator).isDisplayed();
-        } catch (org.openqa.selenium.NoSuchElementException e) {
+        } catch (org.openqa.selenium.NoSuchElementException e){
             return false;
         }
-    }
-    public void visit(String url) {
-        driver.get(url);
     }
 }
